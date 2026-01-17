1e22159d68949db368b17523055379f8206e8817
JasonHuang1103 <jasonmc1103@gmail.com>
2026-01-17 14:33:27 -0800

    update base path

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -12,7 +12,6 @@ export const projectsBlock = {
         }
       ]
     },
-
     {
       title: "CS180: Computer Vision",
       links: [
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/JasonHuang1103.github.io/",
+  base: "./",
   plugins: [react()],
   build: {
     rollupOptions: {
